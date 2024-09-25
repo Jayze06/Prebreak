8ba16ed650f94587e223d5abc2228fd5192e2ca3
Jayze <jz0602@outlook.com>
2024-09-26 03:01:24 +0800

    Initial Commit

---
+++ Mobile prebreak phase/test1/app/src/main/java/com/example/asstest1/screens/Profile.kt
@@ -0,0 +1,33 @@
+package com.example.asstest1.screens
+
+import androidx.compose.foundation.clickable
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import com.example.asstest1.navigation.Routes
+import com.example.asstest1.viewmodel.AuthViewModel
+
+@Composable
+fun Profile(navHostController: NavHostController){
+
+    val authViewModel:AuthViewModel = viewModel()
+    val firebaseUser by authViewModel.firebaseUser.observeAsState(null)
+
+    LaunchedEffect(firebaseUser) {
+        if (firebaseUser==null){
+            navHostController.navigate(Routes.Login.routes){
+                popUpTo(navHostController.graph.startDestinationId)
+                launchSingleTop = true
+            }
+        }
+    }
+
+    Text(text = "Profile", modifier = Modifier.clickable{
+        authViewModel.logout()
+    })
+}
